efa1b4709628bc045ccdfafc7632c338324e0b68
JunYoung Choi <juny0955@gmail.com>
2026-04-14 10:49:07 +0900

    feat: cancelOrder 이벤트 -> outbox 변경

--- services/order-service/src/main/java/dev/junyoung/exchange/orderservice/application/service/CancelOrderService.java
+++ services/order-service/src/main/java/dev/junyoung/exchange/orderservice/application/service/CancelOrderService.java
@@ -1,25 +1,43 @@
 package dev.junyoung.exchange.orderservice.application.service;
 
+import dev.junyoung.exchange.core.exception.CoreException;
+import dev.junyoung.exchange.orderservice.application.exception.OrderErrorCode;
+import dev.junyoung.exchange.orderservice.application.port.out.OrderHistoryRepository;
+import dev.junyoung.exchange.orderservice.application.port.out.OrderOutboxRepository;
+import dev.junyoung.exchange.orderservice.application.port.out.OrderRepository;
+import dev.junyoung.exchange.orderservice.application.service.outbox.OrderOutboxFactory;
+import dev.junyoung.exchange.orderservice.domain.model.entity.OrderHistory;
+import dev.junyoung.exchange.orderservice.domain.model.enums.OrderHisReason;
+import dev.junyoung.exchange.orderservice.domain.model.enums.OrderStatus;
 import org.springframework.stereotype.Service;
 
 import dev.junyoung.exchange.orderservice.application.port.in.CancelOrderUseCase;
 import dev.junyoung.exchange.orderservice.application.port.in.command.CancelOrderCommand;
-import dev.junyoung.exchange.orderservice.application.port.out.OrderEventPublisher;
-import dev.junyoung.exchange.orderservice.application.port.out.command.CancelOrderEvent;
-import dev.junyoung.exchange.orderservice.application.service.tx.CancelOrderTx;
 import dev.junyoung.exchange.orderservice.domain.model.entity.Order;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CancelOrderService implements CancelOrderUseCase {
 
-    private final CancelOrderTx cancelOrderTx;
-    private final OrderEventPublisher orderEventPublisher;
+    private final OrderRepository orderRepository;
+    private final OrderHistoryRepository orderHistoryRepository;
+    private final OrderOutboxRepository orderOutboxRepository;
+    private final OrderOutboxFactory orderOutboxFactory;
 
     @Override
     public void cancelOrder(CancelOrderCommand command) {
-        Order order = cancelOrderTx.cancelOrderTx(command);
-        orderEventPublisher.cancelOrder(new CancelOrderEvent(order.getOrderId().value(), order.getAccountId().value()));
+        Order order = orderRepository.findByIdAndAccountIdForUpdate(command.orderId(), command.accountId())
+            .orElseThrow(() -> new CoreException(OrderErrorCode.ORDER_NOT_FOUND));
+
+        OrderStatus fromStatus = order.getStatus();
+        // TODO PENDING 상태일때 어떻게 처리할지 결정 해야함
+        order.requestCancel();
+
+        orderRepository.updateStatus(order);
+        orderHistoryRepository.save(OrderHistory.createTransition(order, fromStatus, OrderHisReason.USER_REQUEST_CANCEL));
+        orderOutboxRepository.save(orderOutboxFactory.cancelOrder(order));
     }
 }
--- services/order-service/src/main/java/dev/junyoung/exchange/orderservice/application/service/outbox/OrderOutboxFactory.java
+++ services/order-service/src/main/java/dev/junyoung/exchange/orderservice/application/service/outbox/OrderOutboxFactory.java
@@ -1,5 +1,6 @@
 package dev.junyoung.exchange.orderservice.application.service.outbox;
 
+import dev.junyoung.exchange.orderservice.application.port.out.command.CancelOrderEvent;
 import org.springframework.stereotype.Component;
 
 import dev.junyoung.exchange.orderservice.application.port.out.command.PlaceOrderEvent;
@@ -19,5 +20,11 @@ public class OrderOutboxFactory {
 		PlaceOrderEvent event = PlaceOrderEvent.of(order);
 		String payload = objectMapper.writeValueAsString(event);
 		return OrderOutbox.create(order.getOrderId(), EventType.PLACE_ORDER, payload);
+	}
+
+    public OrderOutbox cancelOrder(Order order) {
+		CancelOrderEvent event = new CancelOrderEvent(order.getOrderId().value(), order.getAccountId().value());
+		String payload = objectMapper.writeValueAsString(event);
+		return OrderOutbox.create(order.getOrderId(), EventType.CANCEL_ORDER, payload);
 	}
 }
