7bca4dcc9b8688722199b8561d9174eabc071369
JunYoung Choi <juny0955@gmail.com>
2026-04-15 14:59:18 +0900

    refactor: 불필요 에러코드 삭제

--- services/order-service/src/main/java/dev/junyoung/exchange/orderservice/application/exception/OrderErrorCode.java
+++ services/order-service/src/main/java/dev/junyoung/exchange/orderservice/application/exception/OrderErrorCode.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 public enum OrderErrorCode implements ErrorCode {
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER-NOT-FOUND", "해당 주문을 찾을 수 없습니다."),
     DUPLICATE_PLACE_ORDER(HttpStatus.CONFLICT, "ORDER-DUP-PLACE", "이미 처리된 주문입니다."),
-    ORDER_ALREADY_FINAL(HttpStatus.UNPROCESSABLE_CONTENT, "ORDER-ALREADY-FINAL", "이미 종료된 주문입니다."),
-    ORDER_ALREADY_REQ_CANCEL(HttpStatus.CONFLICT, "ORDER-ALREADY-REQ-CANCEL", "이미 취소 요청된 주문입니다."),
     ;
 
     private final HttpStatus status;
