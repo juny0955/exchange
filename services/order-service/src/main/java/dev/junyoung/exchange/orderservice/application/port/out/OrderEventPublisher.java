0f77a0d27f9726ac6c214663e310fcfb13bc888d
JunYoung Choi <juny0955@gmail.com>
2026-04-16 16:52:14 +0900

    remove: 불필요한 Port 제거
    CDC 방식 사용으로 인한 EventPublisher Port 제거

--- services/order-service/src/main/java/dev/junyoung/exchange/orderservice/application/port/out/OrderEventPublisher.java
+++
@@ -1,9 +0,0 @@
-package dev.junyoung.exchange.orderservice.application.port.out;
-
-import dev.junyoung.exchange.orderservice.application.port.out.command.CancelOrderEvent;
-import dev.junyoung.exchange.orderservice.application.port.out.command.PlaceOrderEvent;
-
-public interface OrderEventPublisher {
-    void placeOrder(PlaceOrderEvent event);
-    void cancelOrder(CancelOrderEvent event);
-}
