0a6efb9155bba521e69599064c3d8b2b1cdeaaa8
Choi Junyoung <juny0955@gmail.com>
2026-04-28 20:44:52 +0900

    feat: account-service init

---
+++ services/account-service/src/main/java/dev/junyoung/exchange/accountservice/AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.junyoung.exchange.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+}
