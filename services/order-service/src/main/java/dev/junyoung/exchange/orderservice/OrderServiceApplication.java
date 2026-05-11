21d5ede1104d9e09ca8ea42c54f9d7f73c8e0242
Choi Junyoung <juny0955@gmail.com>
2026-05-11 21:36:23 +0900

    feat: order-service proto import 제거

--- services/order-service/src/main/java/dev/junyoung/exchange/orderservice/OrderServiceApplication.java
+++ services/order-service/src/main/java/dev/junyoung/exchange/orderservice/OrderServiceApplication.java
@@ -2,11 +2,9 @@ package dev.junyoung.exchange.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.grpc.client.ImportGrpcClients;
 import org.springframework.resilience.annotation.EnableResilientMethods;
 
 @SpringBootApplication(scanBasePackages = "dev.junyoung.exchange")
-@ImportGrpcClients(basePackages = "dev.junyoung.exchange.proto.account.v1")
 @EnableResilientMethods
 public class OrderServiceApplication {
 
